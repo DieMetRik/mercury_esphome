60e47ee846ee3411d202a3fe16eb4a9fde3c6124
Dmitry <samilyanov@gmail.com>
2022-03-20 08:55:17 +0300

    Update mercury-200.02.h
    Переписал опросы и чтение. Теперь тарифы опрашиваются раз в 10 минут, а ток и напряжение раз в 1.

--- esp01s/mercury-200.02.h
+++ esp01s/mercury-200.02.h
@@ -16,7 +16,7 @@ class Mercury : public PollingComponent, public UARTDevice {
 
 	Sensor *CRC_OUT {nullptr};
 
-	int seriall = 001;  // сюда свой серийный номер счетчика
+	int seriall = 319;  // сюда свой серийный номер счетчика
 
 	public:
 	Mercury(UARTComponent *parent, Sensor *sensor1, Sensor *sensor2, Sensor *sensor3, Sensor *sensor4, Sensor *sensor5, Sensor *sensor6, Sensor *sensor7, Sensor *sensor8, TextSensor *sensor9, Sensor *sensor10) : UARTDevice(parent) , Volts(sensor1) , Amps(sensor2) , Watts(sensor3), Tariff1(sensor4), Tariff2(sensor5), Tariff3(sensor6), Sum_Tariff(sensor7), Freq(sensor8), dt_string(sensor9), CRC_OUT(sensor10) {}
@@ -34,6 +34,12 @@ class Mercury : public PollingComponent, public UARTDevice {
 	double T1_f, T2_f, T3_f;
 	uint8_t crc_good_f;
 	bool crc_good[8];
+
+	int cnt_dt_tariff = 1100; // 	Начинать с 1100 при задании 1200 тогда данные по тарифам подтянутся сразу
+
+	int interval = 500; 		// ИНТЕРВАЛ ОБНОВЛЕНИЯ
+	int cnt_dt_tariff_sp = 1200; 	// 600 ПРИ ИНВЕРВАЛЕ 500ms = 5 min
+
 
 	String hh_s, mm_s, ss_s, dd_s, mon_s, yy_s;	//DateTime from Mercury
 	String dt_str;
@@ -177,7 +183,7 @@ class Mercury : public PollingComponent, public UARTDevice {
 
 	void setup() override
 	{
-		this->set_update_interval(2000);	// Периуд обновления сенсора
+		this->set_update_interval(interval);	
 		calculateParams(electrical_parameters, seriall, 0x63);
 		calculateParams(electrical_parameters_F, seriall, 0x81);
 		calculateParams(tarif, seriall, 0x27);
@@ -194,6 +200,7 @@ class Mercury : public PollingComponent, public UARTDevice {
 		write_array(command, 7);
 		delay(12);
 		digitalWrite(0, LOW);
+		//delay(13);
 
 		while (available())
 		{
@@ -207,38 +214,46 @@ class Mercury : public PollingComponent, public UARTDevice {
 
 	void update() override
 	{
-
+		cnt_dt_tariff++;
+//==========================================================================================================
+// ФОРМИРОВАНИЕ ЗАПРОСОВ
 		if (step == 0){
-			main_uart_read(tarif);
+			if (cnt_dt_tariff > cnt_dt_tariff_sp) { 	// ПРОВЕРЯТЬ ДАННЫЕ НЕ КАЖДЫЙ ЦИКЛ
+				main_uart_read(tarif);			// ТАРИФЫ
+			}
 		}
 
 		if (step == 1){
-			main_uart_read(electrical_parameters);
+			main_uart_read(electrical_parameters);		// НАПРЯЖЕНИЕ, ТОК, МОЩНОСТЬ
 		}
 
 		if (step == 2){
-			main_uart_read(electrical_parameters_F);
+			main_uart_read(electrical_parameters_F);	// ЧАСТОТА
 		}
 
-		if (step == 3){
-			main_uart_read(dtime);
+		if (step == 3) {
+			if (cnt_dt_tariff > cnt_dt_tariff_sp) { 	// ПРОВЕРЯТЬ ДАННЫЕ НЕ КАЖДЫЙ ЦИКЛ
+				main_uart_read(dtime);			// ДАТА И ВРЕМЯ
+			}
 		}
 
 		counter = 0;
-
-		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x27)
+//==========================================================================================================
+// ПРОВЕРКА ПОЛУЧЕНЫЙ ДАННЫХ
+// ТАРИФЫ
+		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x27)		
 		{
 
 			if (crc_check(Re_buf, 20)==1) {
 				crc_good[0] = 1;
 				double T1 = readDouble<4> (&Re_buf[5], 100);
-				if (T1 > 0 && T1 < 50000){T1_f = T1;};	// Проверяем данные на адекватность
+				if (T1 > 0 && T1 < 50000){T1_f = T1;};		// Проверяем данные на адекватность
 
 				double T2 = readDouble<4> (&Re_buf[9], 100);
-				if (T2 > 0 && T2 < 50000){T2_f = T2;};	// Проверяем данные на адекватность
+				if (T2 > 0 && T2 < 50000){T2_f = T2;};		// Проверяем данные на адекватность
 
 				double T3 = readDouble<4> (&Re_buf[13], 100);
-				if (T3 > 0 && T3 < 50000){T3_f = T3;};	// Проверяем данные на адекватность
+				if (T3 > 0 && T3 < 50000){T3_f = T3;};		// Проверяем данные на адекватность
 
 				sum = T1_f + T2_f + T3_f;
 			}
@@ -246,47 +261,42 @@ class Mercury : public PollingComponent, public UARTDevice {
 			{
 			crc_good[0] = 0;
 			}
-			step = 1;
 		}
-
-		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x63)
+// НАПРЯЖЕНИЕ, ТОК, МОЩНОСТЬ
+		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x63)		
 		{
-
-			if (crc_check(Re_buf, 11)==1) {			// Проверяем CRC пришедшего сообщения
+			if (crc_check(Re_buf, 11)==1) {				// Проверяем CRC пришедшего сообщения
 				crc_good[1] = 1;
 				double V = readDouble(&Re_buf[5], 10);		// Парсинг байтов и перевод в нормальные значения
-				if (V > 190 && V < 260){V_f = V;};	// Проверяем данные на адекватность
+				if (V > 190 && V < 260){V_f = V;};		// Проверяем данные на адекватность
 
-				double A = readDouble(&Re_buf[7], 100);	// Парсинг байтов и перевод в нормальные значения
-				if (A >= 0 && A < 100){	A_f = A;};	// Проверяем данные на адекватность
+				double A = readDouble(&Re_buf[7], 100);		// Парсинг байтов и перевод в нормальные значения
+				if (A >= 0 && A < 100){	A_f = A;};		// Проверяем данные на адекватность
 
 				double W = readDouble<3> (&Re_buf[9], 1000);	// Парсинг байтов и перевод в нормальные значения
-				if (W >= 0 && W < 100){	W_f = W;};	// Проверяем данные на адекватность
+				if (W >= 0 && W < 100){	W_f = W;};		// Проверяем данные на адекватность
 			}
 			else
 			{
 			crc_good[1] = 0;
 			}
-			step = 2;
 		}
-
-		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x81){
-			if (crc_check(Re_buf, 14)==1) {			// Проверяем CRC пришедшего сообщения
+// ЧАСТОТА
+		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x81){		
+			if (crc_check(Re_buf, 14)==1) {				// Проверяем CRC пришедшего сообщения
 				crc_good[2] = 1;
-				double F = readDouble(&Re_buf[5], 100);	// Парсинг байтов и перевод в нормальные значения
-				if (F >= 20 && F < 80){F_f = F;	};	// Проверяем данные на адекватность
+				double F = readDouble(&Re_buf[5], 100);		// Парсинг байтов и перевод в нормальные значения
+				if (F >= 20 && F < 80){F_f = F;	};		// Проверяем данные на адекватность
 			}
 			else
 			{
 			crc_good[2] = 0;
 			}
-			step = 3;
 
 		}
-
-		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x21){
-			if (crc_check(Re_buf, 11)==1) {			// Проверяем CRC пришедшего сообщения
-				// ДАННЫЕ ПО ВРЕМЕНИ
+// ДАТА И ВРЕМЯ
+		if (Re_buf[0] == 0x00 && Re_buf[4] == 0x21){		
+			if (crc_check(Re_buf, 11)==1) {				// Проверяем CRC пришедшего сообщения
 				crc_good[3] = 1;
 
 				hh_s = readInt(Re_buf[6]);
@@ -297,40 +307,57 @@ class Mercury : public PollingComponent, public UARTDevice {
 				mon_s = readInt(Re_buf[10]);
 				yy_s = "20" + readInt(Re_buf[11]);
 				//------------------
-				dt_str = yy_s + "-" + mon_s + "-" + dd_s + " " + hh_s + ":" + mm_s + ":" + ss_s;	// СФОРМИРОВАТЬ СТРОКУ ВРЕМЕНИ
+				// СФОРМИРОВАТЬ СТРОКУ ВРЕМЕНИ
+				dt_str = yy_s + "-" + mon_s + "-" + dd_s + " " + hh_s + ":" + mm_s + ":" + ss_s;	
 			}
 			else
 			{
 			crc_good[3] = 0;
 			}
+		}
+//==========================================================================================================
+// ФОРМИРУЕМ ШАГ
+		step++;
+		if (step>= 4){
 			step = 0;
 		}
-	
-		crc_good_f = (int)ToByte(crc_good);
-		if (CRC_OUT != nullptr) CRC_OUT->publish_state(crc_good_f);
+//==========================================================================================================
+
+		
+//		crc_good_f = (int)ToByte(crc_good);
+//		if (CRC_OUT != nullptr) CRC_OUT->publish_state(crc_good_f);
+
+//		if (CRC_OUT != nullptr) CRC_OUT->publish_state(cnt_dt_tariff);
 
 
-		// Передача данные в HA
+//==========================================================================================================
+// Передача данные в HA
 
-		// ЕСЛИ ВСЕ ДАННЫЕ ПРИСУТСТВУЮТ, ОПУБЛИКОВАТЬ В HA
-
-		if (V_f > 0){
+// НАПРЯЖЕНИЕ, ТОК, МОЩНОСТЬ
+		if (crc_good[1]==1){
 			if (Volts != nullptr) Volts->publish_state(V_f);	// Отправка в как сенсор
 			if (Amps != nullptr) Amps->publish_state(A_f);
 			if (Watts != nullptr) Watts->publish_state(W_f);
+			crc_good[1] = 0;
+
 		}
-		if (crc_good[0]==1){
+// ТАРИФЫ + ДАТА И ВРЕМЯ
+		if (crc_good[0]==1 && crc_good[3]==1){
 		
 			if (Tariff1 != nullptr) Tariff1->publish_state(T1_f);
 			if (Tariff2 != nullptr) Tariff2->publish_state(T2_f);
 			if (Tariff3 != nullptr) Tariff3->publish_state(T3_f);
 			if (Sum_Tariff != nullptr) Sum_Tariff->publish_state(sum);
-			if (dt_str != ""){
-				if (dt_string != nullptr) dt_string->publish_state(dt_str.c_str()); // ДАТА ЗАПИСЫВАЕТСЯ ЕСЛИ ТОЛЬКО ЕСТЬ ДАННЫЕ ПО ТАРИФАМ
-			}
+			if (dt_string != nullptr) dt_string->publish_state(dt_str.c_str());
+			crc_good[0] = 0;
+			crc_good[3] = 0;
+			cnt_dt_tariff = 0;
 		}
-		if (F_f > 0) {
+// ЧАСТОТА
+		if (crc_good[2]==1) {
 			if (Freq != nullptr) Freq->publish_state(F_f);
+			crc_good[2] = 0;
+
 		}
 	};
 };
